44d60dc5c58d8f3cc80add3ca8d0944f483a44d6
[Nabil Qureshi] <[nabilqureshi446@gmail.com]>
2026-02-18 16:25:19 +0530

    fix-ec2

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,18 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
+}
 
-}
+# -----------------------------
+# EC2 Instance
+# -----------------------------
 resource "aws_instance" "web" {
-    ami = "ami-0c94855ba95c71c99"
-    instance_type = "t3.small"
-    key_name = "3tier"
-    vpc_security_group_ids = ["sg-02a1d14dea6c37136"]
-    tags = {
-        Name = "web"
-        env = "dev"
-    }
+  ami                    = "ami-0c94855ba95c71c99"
+  instance_type          = "t3.small"
+  key_name               = "3tier"
+  vpc_security_group_ids = ["sg-02a1d14dea6c37136"]
+
+  tags = {
+    Name = "web"
+    env  = "dev"
+  }
 }
- 
