243ff319094a4289eab59835a3a8ee447b47c19e
[Nabil Qureshi] <[nabilqureshi446@gmail.com]>
2026-02-18 16:10:09 +0530

    s3-staticwebsite

---
+++ main.tf
@@ -0,0 +1,90 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# -----------------------------
+# EC2 Instance
+# -----------------------------
+resource "aws_instance" "web" {
+  ami                    = "ami-0c94855ba95c71c99"
+  instance_type          = "t3.small"
+  key_name               = "3tier"
+  vpc_security_group_ids = ["sg-02a1d14dea6c37136"]
+
+  tags = {
+    Name = "web"
+    env  = "dev"
+  }
+}
+
+# -----------------------------
+# S3 Bucket
+# -----------------------------
+resource "aws_s3_bucket" "static_site" {
+  bucket = "nabil-static-website-123456"   # change to unique name
+
+  tags = {
+    Name = "StaticWebsite"
+    Env  = "Dev"
+  }
+}
+
+# -----------------------------
+# Enable Static Website Hosting
+# -----------------------------
+resource "aws_s3_bucket_website_configuration" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+# -----------------------------
+# Public Access Block
+# -----------------------------
+resource "aws_s3_bucket_public_access_block" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# -----------------------------
+# Bucket Policy
+# -----------------------------
+resource "aws_s3_bucket_policy" "static_site" {
+
+  depends_on = [aws_s3_bucket_public_access_block.static_site]
+
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = "*"
+      Action = "s3:GetObject"
+      Resource = "${aws_s3_bucket.static_site.arn}/*"
+    }]
+  })
+}
+
+# -----------------------------
+# Upload index.html
+# -----------------------------
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.static_site.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
+
+# -----------------------------
+# Output Website URL
+# -----------------------------
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.static_site.website_endpoint
+}
