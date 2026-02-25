efdc913d8b67fa75c07c7f92da1c25e63c21f211
Nabil Qureshi <nabilqureshi446@gmail.com>
2026-02-25 12:53:02 +0530

    Add security group to EC2 instance configuration fix

--- day3/ec2.tf
+++ day3/ec2.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "my-website" {
     ami = "var.image_id"
     instance_type = "var.instance_type"
     key_name = "var.key_name" 
+    vpc_security_group_ids = ["sg-02a1d14dea6c37136"]
     tags = {
         Name = "my-website"
         env = "dev"
